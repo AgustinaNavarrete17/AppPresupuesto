a657faf260f33cb098d6ce1dbf4baef93b2ae6b6
Agustina Navarrete <agustinanavar1798@gmail.com>
2022-10-04 23:30:12 -0300

    +Comienzo estructura de componentes App presupuesto.

---
+++ src/app/componentes/gastos/gasto-ingresado/gasto-ingresado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gasto-ingresado',
+  templateUrl: './gasto-ingresado.component.html',
+  styleUrls: ['./gasto-ingresado.component.css']
+})
+export class GastoIngresadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/componentes/presupuesto-ingresado/presupuesto-ingresado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-presupuesto-ingresado',
+  templateUrl: './presupuesto-ingresado.component.html',
+  styleUrls: ['./presupuesto-ingresado.component.css']
+})
+export class PresupuestoIngresadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
