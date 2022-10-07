a9020b15130052bd707af18d329d9e4e69b49422
Agustina Navarrete <agustinanavar1798@gmail.com>
2022-10-06 23:15:37 -0300

    +Agrego componentes listado-gastos. Agrego routing para los componentes principales. Doy estilo de fondo. Comienzo a desarrollar el HTML del componente presupuesto-ingresado.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GastosComponent } from './componentes/gastos/gastos.component';
+import { PresupuestoIngresadoComponent } from './componentes/presupuesto-ingresado/presupuesto-ingresado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/ingresarPresupuesto", pathMatch: "full" },
+  { path: "ingresarPresupuesto", component: PresupuestoIngresadoComponent },
+  { path: "gastos", component: GastosComponent },
+  { path: "**", redirectTo: "/ingresarPresupuesto", pathMatch: "full" }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
