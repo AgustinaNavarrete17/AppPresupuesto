8b1b0ed554f9123d36617aece64492e6f1cd3c3c
Agustina Navarrete <agustinanavar1798@gmail.com>
2022-10-19 23:48:24 -0300

    +Comienzo con la lógica del componente presupuesto-ingresado. Comienzo a usar presupuesto.service y presupuesto-ingresado.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
+import { GastosComponent} from './componentes/gastos/gastos.component';
+import { GastoIngresadoComponent} from './componentes/gastos/gasto-ingresado/gasto-ingresado.component';
+import { ListadoGastosComponent } from './componentes/gastos/listado-gastos/listado-gastos.component';
+import { PresupuestoIngresadoComponent } from './componentes/presupuesto-ingresado/presupuesto-ingresado.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosComponent,
+    GastoIngresadoComponent,
+    ListadoGastosComponent,
+    PresupuestoIngresadoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/presupuesto-ingresado/presupuesto-ingresado.component.ts
+++ src/app/componentes/presupuesto-ingresado/presupuesto-ingresado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PresupuestoService } from 'src/app/servicios/presupuesto.service';
 
 @Component({
   selector: 'app-presupuesto-ingresado',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresupuestoIngresadoComponent implements OnInit {
 
-  constructor() { }
+  cantidad : number; 
+  cantidadError : boolean;
+
+  constructor(private presupuestoService : PresupuestoService,
+              private router : Router,) 
+  { 
+    this.cantidad = 0;
+    this.cantidadError = false;
+
+  }
 
   ngOnInit() {
   }
+
+  Agregar(){
+    if (this.cantidad<1){
+      this.cantidadError = true;
+    } else {
+      this.cantidadError = false
+      //seteo en la variable presupuesto del presupuestoService, lo que el usuario ingrese en la variable cantidad
+      this.presupuestoService.presupuesto = this.cantidad;
+      //seteo en la variable restante del presupuestoService, lo que el usuario ingrese en la variable cantidad
+      this.presupuestoService.restante = this.cantidad;
+      //Si no hay error, redirecciono al otro componente
+      this.router.navigate(['/gastos'])
+    }
+  }
+
+
 
 }
