730931e3ba4f3d1dd1ca44642f220d4b394022d5
Agustina Navarrete <agustinanavar1798@gmail.com>
2022-10-22 00:21:10 -0300

    +Avanzo con lógica y maquetado del componente gasto-ingresado

--- src/app/componentes/gastos/gasto-ingresado/gasto-ingresado.component.ts
+++ src/app/componentes/gastos/gasto-ingresado/gasto-ingresado.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gasto-ingresado.component.css']
 })
 export class GastoIngresadoComponent implements OnInit {
+   nombreGasto : string;
+   montoGasto : number;
+   //variable para validar que los datos del formulario sean correctos
+   formIncorrecto : boolean;
+   //variable para validar que el monto del gasto ingresado sea menor al presupuesto ingresado
+   datosIncorrectos : string;
 
-  constructor() { }
+  constructor() { 
+    this.nombreGasto = '';
+    this.montoGasto = 0;
+    this.formIncorrecto = false;
+    this.datosIncorrectos = 'Nombre del gasto o monto incorrectos';
+  }
 
   ngOnInit() {
+  }
+
+  agregarGasto(){
+    if (this.nombreGasto === '' || this.montoGasto <= 0){
+      this.formIncorrecto = true;
+    } else {
+      this.formIncorrecto = false;
+      this.nombreGasto = '';
+      this.montoGasto = 0;
+    }
   }
 
 }
