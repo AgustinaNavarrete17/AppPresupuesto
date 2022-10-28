4f071a9824ea5c636b56dcec5f4a7cc5e3e989fd
Agustina Navarrete <agustinanavar1798@gmail.com>
2022-10-27 22:01:55 -0300

    +Comienzo a trabajar en el maquetado y estilo del componente listado-gastos. También avanzo con la lógica ts del mismo componente.

--- src/app/componentes/gastos/listado-gastos/listado-gastos.component.ts
+++ src/app/componentes/gastos/listado-gastos/listado-gastos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { PresupuestoService } from 'src/app/servicios/presupuesto.service';
 
 @Component({
@@ -8,11 +9,31 @@ import { PresupuestoService } from 'src/app/servicios/presupuesto.service';
 })
 export class ListadoGastosComponent implements OnInit {
 
+  suscription : Subscription;
+  presupuesto : number;
+  presupuestoRestante : number;
+
+  //Lista donde se almacenan todos los gastos:
+  listadoGastos : any [] = [];
+
   constructor(private presupuestoService : PresupuestoService) { 
-    this.presupuestoService.getGastos().subscribe(data =>{console.log(data);})
+    this.presupuesto = 0;
+    this.presupuestoRestante = 0;
+    this.suscription = this.presupuestoService.getGastos().subscribe(data =>{
+    this.presupuestoRestante = this.presupuestoRestante - data.cantidad;  
+    console.log(data)
+    //Aqui pusheo cada gasto en la lista listadoGastos:
+    this.listadoGastos.push(data);  
+    })
   }
 
   ngOnInit() {
+    this.presupuesto = this.presupuestoService.presupuesto;
+    this.presupuestoRestante= this.presupuestoService.restante;
+  }
+
+  ngOnDestroy(): void{
+    this.suscription.unsubscribe();
   }
 
 }
